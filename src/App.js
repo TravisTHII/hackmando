1964eee9d09065d5b96d6f414c63dec5c1a4d927
Travis <travisthackneyii@gmail.com>
2021-01-09 15:52:52 -0500

    Header started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Header } from './components/Header'
 import { Home } from './components/Home'
 import { Projects } from './components/Projects'
 
@@ -8,6 +9,7 @@ import './style/App.css'
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<Home />
 			<Projects />
 		</div>
