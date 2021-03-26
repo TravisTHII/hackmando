79c7837ab0b34cbfaedd228b325d31c312db3c56
unknown <travisthackneyii@gmail.com>
2021-03-26 18:24:49 -0400

    Updated menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,21 +1,66 @@
 import React from 'react'
 
 import { MenuLink } from './MenuLink'
-import { SubMenu } from './SubMenu'
 
 export function Menu() {
+
+	const sub = [
+		{ name: "Comments", slug: "comments", url: "/projects/comments" },
+		{ name: "Search", slug: "search", url: "/projects/search" },
+		{ name: "Queue", slug: "queue", url: "/projects/queue" },
+		{ name: "Discover", slug: "discover", url: "/projects/discover" }
+	]
+
+	const w = /\/projects\/(\w+)/.test(window.location.pathname)
+
 	return (
 		<div className="menu">
 			<div className="menu_container">
+
 				<ul className="menu_list">
+
 					<li className="menu_item">
-						<MenuLink name="Home" slug="home" url="/home" />
+						<MenuLink
+							name="Home"
+							slug="home"
+							url="/home"
+						/>
 					</li>
-					<SubMenu />
+
+					<li className="menu_item sub_menu">
+						<span>
+							<MenuLink
+								name="Projects"
+								slug="comments"
+								url="/projects/comments"
+								active={w}
+							/>
+						</span>
+
+						<ul className="projects_list sub_list">
+							{sub.map((s, i) => (
+								<li className="menu_link-sub">
+									<MenuLink
+										key={i}
+										name={s.name}
+										slug={s.slug}
+										url={s.url}
+									/>
+								</li>
+							))}
+						</ul>
+					</li>
+
 					<li className="menu_item">
-						<MenuLink name="About" slug="about" url="/about" />
+						<MenuLink
+							name="About"
+							slug="about"
+							url="/about"
+						/>
 					</li>
+
 				</ul>
+
 			</div>
 		</div>
 	)
