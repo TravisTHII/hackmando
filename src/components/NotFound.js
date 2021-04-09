5ac7d8a4a704b5a461d1c7c896de1b044d23196b
unknown <travisthackneyii@gmail.com>
2021-04-08 21:44:32 -0400

    Updated notfound page mobile UI

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export function NotFound() {
+
+	useEffect(() => {
+		const x = document.querySelector('.header')
+
+		x.classList.add('is_not_found')
+
+		return () => x.classList.remove('is_not_found')
+	}, [])
+
 	return (
-		<div className="container flex_ui">
-			<div className="not_found_message">
-				<h1>404 Not Found</h1>
-				<p>
-					The page you are looking for does not exist <Link to="/" className="go_home">Go Home.</Link>
-				</p>
+		<div id="not_found">
+			<div className="container flex_ui">
+				<div className="not_found_message">
+					<h1>404 Not Found</h1>
+					<p>
+						The page you are looking for does not exist <Link to="/" className="go_home">Go Home.</Link>
+					</p>
+				</div>
 			</div>
 		</div>
 	)
