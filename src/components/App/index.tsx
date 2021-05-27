5ece42e3e02f2d9e0e4ca2aaf015e050c1aabee3
TravisTHII <travisthackneyii@gmail.com>
2021-05-27 12:29:00 -0400

    Update: dynamic route urls

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { GlobalProvider } from '../../context/Global'
+import { main, projects } from '../../context/Global/data'
 
 import { Header } from '../Header'
 import { Panels } from '../Panels'
 import { NotFound } from '../NotFound'
 
+import { matchString } from '../../utils/matchString'
+
 import '../../style/App.css'
 
-export function App() {
-  return (
-    <GlobalProvider>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" component={Panels} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    </GlobalProvider>
-  )
-}
+export const App = () =>
+  <GlobalProvider>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path={`/(|${matchString(main)})`} component={Panels} />
+        <Route path={`/projects/(${matchString(projects)})`} component={Panels} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </GlobalProvider>
