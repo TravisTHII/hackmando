eb3336d2999cbdfb0d555863248d0af9c2c59640
TravisTHII <travisthackneyii@gmail.com>
2021-05-27 09:39:01 -0400

    Updated styling

--- src/components/Panes/Home/index.tsx
+++ src/components/Panes/Home/index.tsx
@@ -8,12 +8,12 @@ import { skills } from './skills'
 export const Home = () =>
   <div className="container flex_ui">
 
-    <div className="card grid_normal">
+    <div className="home_card">
 
-      <div className="grid_one card_ui">
-        <div className="content_header">
+      <div>
+        <div className="home_header">
           <h1>Travis Hackney</h1>
-          <p>Javascript Developer</p>
+          <p>Web Developer</p>
         </div>
         <div className="content_image flex_ui">
           <div className="content_picture">
@@ -25,27 +25,25 @@ export const Home = () =>
         </div>
       </div>
 
-      <div className="grid_two card_ui">
-        <div className="home_card">
-          <div className="card_header">
-            <h2>Hello,</h2>
-          </div>
-          <div className="home_content">
-            <p>
-              I have been learning web development since as early as 2015,
-              developing and building full stack applications.
+      <div className="portfolio">
+        <div>
+          <h2>Hello,</h2>
+        </div>
+        <div className="portfolio_content">
+          <p>
+            I have been learning web development since as early as 2015,
+            developing and building full stack applications.
           </p>
-            <p>
-              Interacting with a plethora of technologies and taking advantage
-              of their quirks to solve application requirements is challenging and rewarding.
+          <p>
+            Interacting with a plethora of technologies and taking advantage
+            of their quirks to solve application requirements is challenging and rewarding.
           </p>
-            <p>
-              I love how programming enables me to solve problems.
+          <p>
+            I love how programming enables me to solve problems.
           </p>
-          </div>
-          <div className="card_tech">
-            <BuiltWith builtWith={skills} />
-          </div>
+        </div>
+        <div className="card_tech flex_ui">
+          <BuiltWith builtWith={skills} />
         </div>
       </div>
 
