e813c2288e647ea05109e749d14626a386194baa
TravisTHII <travisthackneyii@gmail.com>
2021-12-07 18:17:54 -0500

    updated notfound page

--- src/components/notfound/index.tsx
+++ src/components/notfound/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const NotFound = () => {
-  return (
-    <div id="not_found">
-      <div className="container">
-        <h1>Not Found...</h1>
+import Svg from '../../svg/not_found.svg'
+
+export const NotFound = () => (
+  <div id="not_found">
+    <div className="not_found_container">
+      <div className="not_found_message">
+        <div className="not_found_header">
+          <h1>
+            <span className="blue_highlight">404</span> Not Found
+          </h1>
+          <p>
+            The page you are looking for does not exist{' '}
+            <Link to="/" className="blue_highlight">
+              Go Home.
+            </Link>
+          </p>
+        </div>
+        <div className="not_found_image">
+          <img src={Svg} alt="not found tree" />
+        </div>
       </div>
     </div>
-  )
-}
+  </div>
+)
