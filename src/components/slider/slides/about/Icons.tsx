225ef2c57bb101571930f343a09ce79457991d44
TravisTHII <travisthackneyii@gmail.com>
2021-12-07 09:09:59 -0500

    updated icons

--- src/components/slider/slides/about/Icons.tsx
+++ src/components/slider/slides/about/Icons.tsx
@@ -1,12 +1,14 @@
-import { FaNodeJs, FaReact, FaCss3 } from 'react-icons/fa'
 import {
   SiMongodb,
   SiTypescript,
   SiJavascript,
   SiPhp,
   SiPython,
+  SiHtml5,
+  SiNodedotjs,
+  SiReact,
+  SiCss3,
 } from 'react-icons/si'
-import { AiFillHtml5 } from 'react-icons/ai'
 
 interface Props {
   name: string
@@ -15,12 +17,12 @@ interface Props {
 export function Icons({ name }: Props) {
   let icon
 
-  if (name === 'React') icon = <FaReact title={name} />
-  if (name === 'Node.js') icon = <FaNodeJs title={name} />
+  if (name === 'React') icon = <SiReact title={name} />
+  if (name === 'Node.js') icon = <SiNodedotjs title={name} />
   if (name === 'MongoDB') icon = <SiMongodb title={name} />
   if (name === 'Typescript') icon = <SiTypescript title={name} />
-  if (name === 'HTML') icon = <AiFillHtml5 title={name} />
-  if (name === 'CSS') icon = <FaCss3 title={name} />
+  if (name === 'HTML') icon = <SiHtml5 title={name} />
+  if (name === 'CSS') icon = <SiCss3 title={name} />
   if (name === 'Javascript') icon = <SiJavascript title={name} />
   if (name === 'PHP') icon = <SiPhp title={name} />
   if (name === 'Python') icon = <SiPython title={name} />
