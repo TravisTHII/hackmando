1a20993dc439d51e87308ce1b0797275819852d3
unknown <travisthackneyii@gmail.com>
2022-04-22 17:31:20 -0400

    updated project view list links

--- src/components/slider/slides/projects/view/list/ListItem.tsx
+++ src/components/slider/slides/projects/view/list/ListItem.tsx
@@ -12,27 +12,28 @@ export const ListItem = ({
   techStack,
   year,
 }: Project) => (
-  <li className="projects_list_item white_border">
-    <a href={url} className="flex_ui" target="_blank" rel="noopener noreferrer">
+  <li>
+    <a
+      href={url}
+      className="projects_list_item white_border flex_ui"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <div className="pl_icon">
         <img src={icon} alt={title} />
       </div>
+      <div className="pl_info">
+        <div className="pli_header">
+          <h2>{title}</h2>
+          <p>{year}</p>
+        </div>
+        <div className="pli_sub_header">
+          <p>{subTitle}</p>
+        </div>
+        <ul className="pli_tech_stack">
+          <TechStack techStack={techStack} size="1.5em" />
+        </ul>
+      </div>
     </a>
-    <div className="pl_info">
-      <div className="pli_header">
-        <h2>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {title}
-          </a>
-        </h2>
-        <p>{year}</p>
-      </div>
-      <div className="pli_sub_header">
-        <p>{subTitle}</p>
-      </div>
-      <ul className="pli_tech_stack">
-        <TechStack techStack={techStack} size="1.5em" />
-      </ul>
-    </div>
   </li>
 )
